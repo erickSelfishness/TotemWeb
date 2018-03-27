21c90d667a8af9ebd5bf8d67dd19afcde35f8843
ECARVALLO\Erick <erickc@selfishness.cl>
2018-03-27 03:39:49 +0200

    modificando valores standars de la pagina -2

--- protected/config/config_local.php
+++ protected/config/config_local.php
@@ -1,9 +1,9 @@
 <?php
 
-$dbhost = "aazi5v3kn45zad.cavwsztvupxj.us-east-1.rds.amazonaws.com";
-$dbname = "estacion_sueno2";
+$dbhost = "localhost";
+$dbname = "estacion_sueno3";
 $dbuser = "root";
-$dbpass = "Selftech_123*cato";
+$dbpass = "";
 
 $preload = array(
     'log',

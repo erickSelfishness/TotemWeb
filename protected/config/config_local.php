42b48d82919709565399d9abc3993c5110d3d7cc
felipe huerta <felipe.huerta.iturra@gmail.com>
2018-04-01 06:24:27 +0000

    modificacion de css para mejor navegacion, realizado por Felipe, se cambia el archivo config para probarlo en server prd

--- protected/config/config_local.php
+++ protected/config/config_local.php
@@ -1,12 +1,12 @@
 <?php
 
-$dbhost = "localhost";
-$dbname = "estacion_sueno3";
+$dbhost = "aazi5v3kn45zad.cavwsztvupxj.us-east-1.rds.amazonaws.com";
+$dbname = "estacion_sueno2";
 $dbuser = "root";
-$dbpass = "PQ2vHP1T3xua";
+$dbpass = "Selftech_123*cato";
 
 $preload = array(
     'log',
     'bootstrap',
-    'less', // REMOVE IN PRODUCTION
+//    'less', // REMOVE IN PRODUCTION
 );

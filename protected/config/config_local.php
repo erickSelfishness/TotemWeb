79cf1c8c533cac6c019483008f4c1b16c5507dec
ECARVALLO\Erick <erickc@selfishness.cl>
2018-04-05 05:17:49 +0200

    Esperando que sea la version correcta

--- protected/config/config_local.php
+++ protected/config/config_local.php
@@ -2,11 +2,10 @@
 $dbhost = "localhost";
 $dbname = "estacion_sueno3";
 $dbuser = "root";
-$dbpass = "PQ2vHP1T3xua";
+$dbpass = "";
 
 $preload = array(
     'log',
     'bootstrap',
-    //'less', // REMOVE IN PRODUCTION
+    'less', // REMOVE IN PRODUCTION
 );
-
